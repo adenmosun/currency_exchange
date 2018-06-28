08f4df52383212399a6b46cbfeb7b90b020ce04b
temitayo <tayo.adenmosun@gmail.com>
2018-06-28 13:13:29 +0100

    fixed sevice worker

--- sw.js
+++ sw.js
@@ -1,16 +1,15 @@
 
 
 
-
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(exchangecurrency).then(function(cache) {
+    caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/',
-        '/main.js',
-        '/main.css',
-        '/index.html',
-        '/image/SideImage.JPG'
+        './',
+        './main.js',
+        './main.css',
+        './index.html',
+        './image/SideImage.JPG'
       ]);
     })
   );
@@ -18,8 +17,16 @@ self.addEventListener('install', function(event) {
 
 
 self.addEventListener('fetch', function(event) {
+  // Calling event.respondWith means we're in charge
+  // of providing the response. We pass in a promise
+  // that resolves with a response object
   event.respondWith(
+    // First we look for something in the caches that
+    // matches the request
     caches.match(event.request).then(function(response) {
+      // If we get something, we return it, otherwise
+      // it's null, and we'll pass the request to
+      // fetch, which will use the network.
       return response || fetch(event.request);
     })
   );
